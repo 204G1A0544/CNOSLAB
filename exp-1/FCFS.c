e7bad35eb97a74e04b65a4ad5e710df01310891d
204G1A0544 <94515012+204G1A0544@users.noreply.github.com>
2022-09-13 12:57:27 +0530

    A.Jayasree  Created FCFS.c

---
+++ exp-1/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A.Jayasree
+   Roll No: 204G1A0544
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
+*/
