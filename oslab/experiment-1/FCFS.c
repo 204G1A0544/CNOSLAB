140472c4c2747d7408dd363b42d6362e4f3de5e6
204G1A0544 <94515012+204G1A0544@users.noreply.github.com>
2022-09-26 16:07:53 +0530

    A.Jayasree Created FCFS.c"

---
+++ oslab/experiment-1/FCFS.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: A.Jayasree
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 26-09-2022
+   Date of Execution: 26-092022
+*/
