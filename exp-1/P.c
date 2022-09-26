eadad94aeed87bc986293f4fcaec448e0c9b263c
204G1A0544 <94515012+204G1A0544@users.noreply.github.com>
2022-09-26 16:28:15 +0530

    A.Jayasree created P.c

---
+++ exp-1/P.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A.Jayasree
+   Roll No: 204G1A0544
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 26-09-2022
+   Date of Execution: 26-09-2022
+*/
