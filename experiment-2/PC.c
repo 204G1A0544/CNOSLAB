afc5fb9b036a41cc482f93957d4d3ab5ad0de7eb
204G1A0544 <94515012+204G1A0544@users.noreply.github.com>
2022-10-18 11:56:41 +0530

    A.Jayasree created PC.c

---
+++ experiment-2/PC.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: A.Jayasree
+   Roll No: 204G1A0544
+   Title of the Experiment:To simulate producer-consumer problem using semaphores.
+   Date of Creation: 18-10-2022
+   Date of Execution: 18-10-2022
+*/
